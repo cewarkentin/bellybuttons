07cf7c65ed8aebc880941936157f8a80c4147743
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-07-11 16:35:27 -0500

    this is the right index?

--- js/charts.js
+++ js/charts.js
@@ -58,7 +58,7 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+    
     // 4. Create a variable that filters the samples for the object with the desired sample number.
 
     //  5. Create a variable that holds the first sample in the array.
@@ -78,9 +78,10 @@ function buildCharts(sample) {
     ];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title: "Top 10 Bacteria Cultures Found",
     };
-    // 10. Use Plotly to plot the data with the layout. 
     
+    // 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("plotArea", [barData], barLayout);
   });
 }
