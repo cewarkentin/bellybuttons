b7f93af725edd797affdb3af1607c325c6340e89
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-07-12 19:28:57 -0500

    Update charts.js

--- charts.js
+++ charts.js
@@ -30,22 +30,26 @@ function optionChanged(newSample) {
   
 }
 
-// Demographics Panel
+// Demographics Panel 
 function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
+    // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
+    // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
 
+    // Use `.html("") to clear any existing metadata
     PANEL.html("");
-    PANEL.append("h6").text("ID:" + result.id);
-    PANEL.append("h6").text("ETHNICITY:" + result.ethnicity);
-    PANEL.append("h6").text("GENDER:" + result.gender);
-    PANEL.append("h6").text("AGE:" + result.age);
-    PANEL.append("h6").text("LOCATION:" + result.location);
-    PANEL.append("h6").text("BBTYPE:" + result.bbtype);
-    PANEL.append("h6").text("WFREQ:" + result.wfreq);
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+
   });
 }
 
