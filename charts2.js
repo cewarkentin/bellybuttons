0fae5be69ae600866a2d7ad758829cc9cbcb5f93
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-07-12 21:09:10 -0500

    deliverable 3

--- charts2.js
+++ charts2.js
@@ -78,6 +78,8 @@ function buildCharts(sample) {
     }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
+      width: 500,
+      height: 500,
       title: "Top 10 Bacteria Cultures Found",
       hovermode: "closest"
     };
@@ -108,17 +110,41 @@ function buildCharts(sample) {
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
 
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    // 2. Create a variable that holds the first sample in the metadata array.
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+
+    // 3. Create a variable that holds the washing frequency.
+    var wfreq = parseFloat(result.wfreq);
+
     // 4. Create the trace for the gauge chart.
-    var gaugeData = [
-      
-    ];
+    var gaugeData = [{
+      value: wfreq,
+      type: "indicator",
+      mode: "gauge+number",
+      title: {text: "<b> Belly Button Washing Frequency </b> <br> Scrubs Per Week </br>"},
+      gauge: {
+        axis: {range: [null, 10], tickwidth: 1},
+        bar: {color: "black"},
+        steps: [
+          {range: [0,2], color: "red"},
+          {range: [2,4], color: "orange"},
+          {range: [4,6], color: "yellow"},
+          {range: [6,8], color: "yellowgreen"},
+          {range: [8,10], color: "darkgreen"}
+        ],
+      },
+    }];
     
     // 5. Create the layout for the gauge chart.
-    var gaugeLayout = { 
-    
+    var gaugeLayout = {
+      width: 500,
+      height: 500
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
-    Plotly.newPlot();
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 }
